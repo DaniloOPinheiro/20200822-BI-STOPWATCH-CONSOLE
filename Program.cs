9775b0fda00c1272964696dbe7a617d5f4601ea6
DaniloOPinheiro <danilopinheiro88@icloud.com>
2020-08-25 21:22:21 -0300

    App Cronômentro Console

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+            Start(10);
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S - Segundo");
+            Console.WriteLine("M - Minuto");
+            Console.WriteLine("0 - Sair");
+            Console.WriteLine("Quanto tempo deseja contar ?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = Char.Parse(data.Substring(data.Length -1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            
+            // Console.WriteLine(data);
+            Console.WriteLine(type);
+            Console.WriteLine(time);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(100);
+            }
+            Console.Clear();
+            Console.WriteLine("Stopwatch finish");
+            Thread.Sleep(200);
+
+        }
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Fundamentals CSHARP
+
+* Learn or consolidate all the knowledge needed to work with C # and .NET!
+
+
+		Using the project via console, with solid bases in csharp.
+
+## CREATING A STOPWATCH VIA CONSOLE
+
+# Fundamentos em CSHARP
+
+* Aprenda ou consolide todo o conhecimento necessário para trabalhar com C # e .NET!
+
+
+        Utilizando o projeto via console, com bases sólidas em csharp.
+
+## CRIAÇÃO DE UM CRONÔMETRO VIA CONSOLE
